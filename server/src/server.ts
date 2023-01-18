11071de4012e0fa87c198cd4bdd72f83d383a073
Nicolas Ribeiro <commit@niskii.dev>
2023-01-17 21:04:07 -0300

    First Commit

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import cors from '@fastify/cors';
+import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors)
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits;
+});
+
+app.listen({ port: 3000 }).then(() => console.log("Server is running"));
---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css';
+
+import { Habit } from "./components/Habit";
+
+function App() {
+  return <Habit completed={20} />;
+}
+
+export default App;
